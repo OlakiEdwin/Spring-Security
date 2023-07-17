818a99ed42b3f9973194ca857a8b069144c7ff45
Olaki Edwin <olakiedwin@gmail.com>
2023-07-17 10:52:02 +0300

    User Detsild service

--- src/main/java/com/example/springsecurityjwt/SecurityConfigurer.java
+++ src/main/java/com/example/springsecurityjwt/SecurityConfigurer.java
@@ -1,10 +1,15 @@
 package com.example.springsecurityjwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
    @Autowired
     private MyUserDetailsService myUserDetailsService;
@@ -12,5 +17,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
